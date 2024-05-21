06429916d2b46315f8ee08b30527430fc6ea0e3f
Raymond <raymond.hoang@water.ca.gov>
2024-05-21 13:47:43 -0700

    add dockerfile and run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activate environment
+source /env/bin/activate
+
+git_repo_url="https://github.com/$1.git"
+
+# Clone the repo into a subfolder
+echo "cloning repo $1"
+git clone -b production "$git_repo_url" "code"
+
+# Run app
+echo "Running the application..."
+
+flask run --host 0.0.0.0 --port 80
