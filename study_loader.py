32f6025e5c44b49ae02709931bf5e218c6d508bb
Raymond <raymond.hoang@water.ca.gov>
2024-06-05 13:22:26 -0700

    rename stand-alone study loader

---
+++ study_loader.py
@@ -0,0 +1,27 @@
+from utils.tools import load_data_mult
+from collections import namedtuple
+import yaml
+import pandas as pd
+
+# Scenario management
+Scenario = namedtuple('Scenario',['pathname','alias','active'])
+with open('constants/vars.yaml', 'r') as file:
+    var_dict = yaml.safe_load(file)
+
+s1 = Scenario('dss_files/DCRBL_DV_6.68.dss', 'DCR_21_Hist',1)
+s2 = Scenario('dss_files/DCR2023_DV_9.0.0_Danube_Adj_v1.2.dss', 'DCR_23_Adj',1)
+s3 = Scenario('dss_files/DCR2023_DV_9.0.0_Danube_CC50_v1.2.1.dss', 'DCR_23_CC50',1)
+s4 = Scenario('dss_files/DCR2023_DV_9.0.0_Danube_CC75_v1.2.1.dss', 'DCR_23_CC75',1)
+s5 = Scenario('dss_files/DCR2023_DV_9.0.0_Danube_CC95_v1.2.1.dss', 'DCR_23_CC95',1)
+s6 = Scenario('dss_files/DCR2023_DV_9.0.0_Danube_Hist_v1.1.dss', 'DCR_23_Hist',1)
+
+date_map = pd.read_csv('constants/date_map.csv', index_col=0, parse_dates=True)
+
+scenarios = (scenario for scenario in [s1,s2,s3,s4,s5,s6] if scenario.active==1)
+
+scen_dict={}
+for s in scenarios:
+    if s.active==1:
+        scen_dict[s.alias] = s.pathname
+
+load_data_mult(scen_dict,var_dict,date_map)
