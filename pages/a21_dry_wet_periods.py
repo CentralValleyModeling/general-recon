38ccd85fbf31e7cba60819598ecba13e9b6646b6
Zachary Roy <zachary.roy@water.ca.gov>
2024-07-18 15:07:05 -0700

    Add download buttons

--- pages/a21_dry_wet_periods.py
+++ pages/a21_dry_wet_periods.py
@@ -1,8 +1,8 @@
 import dash_bootstrap_components as dbc
-from dash import dcc, html, register_page
+from dash import Input, Output, callback, dcc, html, register_page
 
 from charts.chart_layouts import a21_dry_wet_barplot
-from pages.styles import GLOBAL_MARGIN
+from data import create_download_button, universal_data_download
 from utils.query_data import df_dv, scen_aliases
 from utils.tools import common_pers
 
@@ -12,6 +12,8 @@ register_page(
     top_nav=True,
     path="/a21_dry_wet_periods",
 )
+DWNLD_DRY_ID = "article-21-dry-years"
+DWNLD_WET_ID = "article-21-wet-years"
 
 
 def layout():
@@ -41,6 +43,8 @@ def layout():
         scens=scen_aliases,
         perlist=drypers,
     )
+
+    button_download_dry = create_download_button(DWNLD_DRY_ID, dry_pers)
     wet_pers = a21_dry_wet_barplot(
         df_dv,
         common_pers,
@@ -49,10 +53,13 @@ def layout():
         perlist=wetpers,
     )
 
+    button_download_wet = create_download_button(DWNLD_WET_ID, wet_pers)
+
     dry_div = dbc.Row(
         [
             html.H3("Article 21 Deliveries during dry periods"),
             dcc.Graph(figure=dry_pers),
+            button_download_dry,
         ],
         class_name="m-3",
     )
@@ -60,8 +67,24 @@ def layout():
         [
             html.H3("Article 21 Deliveries during wet periods"),
             dcc.Graph(figure=wet_pers),
+            button_download_wet,
         ],
         class_name="m-3",
     )
-    layout = dbc.Container([dbc.Col([dry_div, wet_div])])
+    layout = dbc.Container(
+        [
+            dcc.Download(id="download-response-article-21"),
+            dbc.Col([dry_div, wet_div]),
+        ]
+    )
     return layout
+
+
+@callback(
+    Output("download-response-article-21", "data"),
+    Input(DWNLD_DRY_ID, "n_clicks"),
+    Input(DWNLD_WET_ID, "n_clicks"),
+    prevent_initial_call=True,
+)
+def home_data_download(*args):
+    return universal_data_download()
