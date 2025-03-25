f7b07adbf7c5dd561ee05fa0cd885f1f6fcb87a8
Raymond <raymond.hoang@water.ca.gov>
2025-03-25 09:54:17 -0700

    update colors and photo

--- app.py
+++ app.py
@@ -50,13 +50,48 @@ pages = [
     for page in dash.page_registry.values()
 ]
 
-navbar = dbc.NavbarSimple(
-    brand=ReconNavbarBrand("SWP Climate Adaptation Plan"),
-    children=pages,
-    color="light",
-    dark=False,
-    className="mb-0",
+#navbar = dbc.NavbarSimple(
+#    brand=ReconNavbarBrand("SWP Climate Adaptation Plan"),
+#    children=pages,
+#    #color="light",
+#    #dark=False,
+#    className="mb-0",
+#    style={"backgroundColor": "#007fbd",
+#           "color": "white"}
+#)
+
+navbar = dbc.Navbar(
+    dbc.Container([
+        dbc.NavbarBrand("SWP Climate Adaptation Plan", href="/", style={"color": "white"}),
+
+        dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+
+        dbc.Collapse(
+            dbc.Nav(pages,
+                    className="ms-auto",
+                    navbar=True
+                    ),
+            id="navbar-collapse",
+            is_open=False,
+            navbar=True,
+        )
+    ]),
+    color="#007fbd",  # still needs custom CSS for full override
+    dark=True,
+    sticky="top"
 )
+
+# Callback to handle toggling collapse on mobile
+@app.callback(
+    dash.Output("navbar-collapse", "is_open"),
+    dash.Input("navbar-toggler", "n_clicks"),
+    dash.State("navbar-collapse", "is_open"),
+)
+def toggle_navbar(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 
 
 app.layout = html.Div(
