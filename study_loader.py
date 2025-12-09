9aca885b84d5905575c55a8da9b9d5646651f35f
Auhona Zaki <Auhona.Zaki@water.ca.gov>
2025-12-09 09:42:58 -0800

    added 2025 CC75 study to recon

--- study_loader.py
+++ study_loader.py
@@ -45,7 +45,11 @@ studies = [
 
     Study(r"C:\jobs\2025_DCR25\models\3.7.0_danube_cc95\DSS\output\CCA5_v3.7.0__2043_95CC_LU100_SLR30_20241227.dss",
           r"C:\jobs\2025_DCR25\models\3.7.0_danube_cc95\DSS\input\DCR2023_SV_Danube_2043_cc95_v1.9.dss",
-          "DCR25_CC95", "DCR 2025", "2043_CC95", 1)
+          "DCR25_CC95", "DCR 2025", "2043_CC95", 1),
+
+    Study(r"C:\jobs\2025_DCR25\models\3.7.0_danube_cc75\DSS\output\CCA_v3.7.0__2043_75CC_LU100_SLR30_20241227.dss",
+          r"C:\jobs\2025_DCR25\models\3.7.0_danube_cc75\DSS\input\DCR2023_SV_Danube_cc75_v1.2.1.dss",
+          "DCR25_CC75", "DCR 2025", "2043_CC75", 1)
 
 ]
 
